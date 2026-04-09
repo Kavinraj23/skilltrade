cc674d94f5d3adea5d2c3009dd4890699453359b
Kavin Raj <kavin.raj@outlook.com>
2026-04-09 00:08:37 -0500

    initial commit

---
+++ SkillTrade/SkillTradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillTradeApp.swift
+//  SkillTrade
+//
+//  Created by Raj, Kavin on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct SkillTradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
